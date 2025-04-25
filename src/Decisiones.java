8776ba2a25f47ad82b21dd8557243f53b13ac28d
kalchaqui <diego.rbarrionuevo@gmail.com>
2025-04-24 23:24:49 -0300

    Primer commit

---
+++ src/Decisiones.java
@@ -0,0 +1,22 @@
+public class Decisiones {
+
+    public static void main(String[] args) {
+        int fechaDeLanzamiento = 1999;
+        boolean incluioEnElPlan = false;
+        double notaDeLaPelicula = 8.2;
+        String tipoPlan = "plus";
+
+        if (fechaDeLanzamiento >= 2024) {
+            System.out.println("Peliculas mas populares");
+        }else {
+            System.out.println("Peliculas retro que aún valen la pena ver");
+        }
+        if(incluioEnElPlan || tipoPlan.equals("plus")){ //Si es veradera o si el tipo de plan es igual a "plus"
+            System.out.println("Disfrutá de la película");
+        }else {
+            System.out.println("Pelicula no displonible para su plan actual");
+        }
+        }
+    }
+
+
--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,32 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Bienvenido Kalcha a Java");
-        System.out.println("");
+        System.out.printf("Bienvenido a Screen March");
+        System.out.println("Película: Matrix");
 
+       int fechaDeLanzamiento = 1999;
+       boolean incluioEnElPlan = true;
+       double notaDeLaPelicula = 8.2;
+
+       double media = (8.2 + 6.0 + 9.0) / 3 ;
+       System.out.println(media);
+
+       String sinopsis = """
+               Matrix es una paradoja
+               La mejor película del fin del milenio
+               Fué lanzada en:
+               """ + fechaDeLanzamiento ;
+       System.out.println(sinopsis);
+
+       int clasificacion = (int) (media / 2);
+       System.out.println(clasificacion);
+
+        String saludo = "Hola, mi nombre es ";
+        String nombre = "Alicia y tengo  ";
+        int    edad = 17;
+        String continuacion = saludo + nombre + edad;
+
+        System.out.println(continuacion);
 
     }
 }
